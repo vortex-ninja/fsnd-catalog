a053beb3b17b2a65d46cbf374c0dc9e3ea413802
vortex-ninja <vortex.ninja@protonmail.com>
2017-06-09 00:45:40 +0200

    feat: Add CRUD functionality

--- final-project.py
+++ final-project.py
@@ -1,34 +1,39 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_bootstrap import Bootstrap
 from forms import CreateResForm, EditResForm, DeleteForm
 from forms import AddMenuItemForm, EditMenuItemForm
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 Bootstrap(app)
 
-
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
+    restaurants = session.query(Restaurant).all()
+    if not restaurants:
+        flash('The restaurant list is empty.')
     return render_template('restaurants.html', restaurants=restaurants)
 
 
 @app.route('/restaurant/new', methods=['GET', 'POST'])
 def newRestaurant():
     form = CreateResForm()
-    if form.validate_on_submit():  # checks whether it's a POST request and if data is valid
-        pass
+
+    # validate_on_submit checks whether it's a POST request and if data is valid
+    if form.validate_on_submit():
+        new_restaurant = Restaurant(name=form.name.data)
+        session.add(new_restaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
 
     return render_template('newrestaurant.html', form=form)
 
@@ -37,82 +42,101 @@ def newRestaurant():
 def editRestaurant(restaurant_id):
 
     form = EditResForm()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if form.validate_on_submit():
-        pass
+        restaurant.name = form.name.data
+        session.commit()
+        return redirect(url_for('showRestaurants'))
 
-    print(restaurant_id)
-    for r in restaurants:
-        if r['id'] == str(restaurant_id):
-            name = r['name']
-    return render_template('editrestaurant.html', form=form, name=name)
+    return render_template('editrestaurant.html', form=form, name=restaurant.name)
 
 
 @app.route('/restaurant/<int:restaurant_id>/delete', methods=['GET', 'POST'])
 def deleteRestaurant(restaurant_id):
     form = DeleteForm()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if form.validate_on_submit():
-        pass
+        session.delete(restaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
 
-    for r in restaurants:
-        if r['id'] == str(restaurant_id):
-            name = r['name']
-    return render_template('deleterestaurant.html', form=form, name=name)
+    return render_template('deleterestaurant.html', form=form, name=restaurant.name)
 
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return render_template('menu.html', restaurant_id=restaurant_id, items=items)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    if not items:
+        flash('Menu item list is empty.')
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     form = AddMenuItemForm()
-    if form.validate_on_submit():
-        pass
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
 
-    for r in restaurants:
-        if r['id'] == str(restaurant_id):
-            name = r['name']
+    if form.validate_on_submit():
+        item = MenuItem(name=form.name.data,
+                        course=form.course.data,
+                        description=form.description.data,
+                        price=form.price.data,
+                        restaurant_id=restaurant_id)
+        session.add(item)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+
     return render_template('newmenuitem.html',
                            form=form,
-                           name=name,
-                           restaurant_id=restaurant_id)
+                           restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
     form = EditMenuItemForm()
-    form.course.data = 'Beverage'
-    if form.validate_on_submit():
-        pass
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
 
-    for r in restaurants:
-        if r['id'] == str(restaurant_id):
-            name = r['name']
+    if form.validate_on_submit(): # How to assign values more consicely
+        item.name = form.name.data
+        item.course = form.course.data
+        item.description = form.description.data
+        item.price = form.price.data
+
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+
+    form.name.data = item.name
+    form.course.data = item.course
+    form.description.data = item.description
+    form.price.data = item.price
+
     return render_template('editmenuitem.html',
                            form=form,
-                           restaurant=name,
-                           menu_item='gryzka',
-                           restaurant_id=restaurant_id)
+                           restaurant=restaurant,
+                           item=item)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
     form = DeleteForm()
-    if form.validate_on_submit():
-        pass
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
 
-    for r in restaurants:
-        if r['id'] == str(restaurant_id):
-            name = r['name']
+    if form.validate_on_submit():
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+
     return render_template('deletemenuitem.html',
                            form=form,
-                           menu_item='gryzka',
-                           restaurant_id=restaurant_id)
+                           restaurant=restaurant,
+                           item=item)
 
 
 if __name__ == '__main__':
     app.secret_key = 'Top secret key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)

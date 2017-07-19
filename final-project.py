42f2a81f97ad474977ee37315997a146de1435bf
vortex-ninja <vortex.ninja@protonmail.com>
2017-07-20 00:06:50 +0200

    refactor: Adjust views for new functionality

--- final-project.py
+++ final-project.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from flask import jsonify
 from flask_bootstrap import Bootstrap
-from forms import CreateResForm, EditResForm, DeleteForm
-from forms import AddMenuItemForm, EditMenuItemForm
+from forms import CreateCategoryForm, EditCategoryForm, DeleteForm
+from forms import AddItemForm, EditItemForm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
@@ -26,146 +26,139 @@ def mainPage():
     return render_template('main.html', categories=categories, items=items)
 
 
-# @app.route('/restaurant/new', methods=['GET', 'POST'])
-# def newRestaurant():
-#     form = CreateResForm()
+@app.route('/category/new', methods=['GET', 'POST'])
+def newCategory():
+    form = CreateCategoryForm()
 
-#     # validate_on_submit checks whether it's a POST request and if data is valid
-#     if form.validate_on_submit():
-#         new_restaurant = Restaurant(name=form.name.data)
-#         session.add(new_restaurant)
-#         session.commit()
-#         flash('New restaurant created.')
-#         return redirect(url_for('showRestaurants'))
+    # validate_on_submit checks whether it's a POST request and if data is valid
+    if form.validate_on_submit():
+        new_category = Category(name=form.name.data)
+        session.add(new_category)
+        session.commit()
+        flash('New category created.')
+        return redirect(url_for('mainPage'))
 
-#     return render_template('newrestaurant.html', form=form)
-
-
-# @app.route('/restaurant/<int:restaurant_id>/edit', methods=['GET', 'POST'])
-# def editRestaurant(restaurant_id):
-
-#     form = EditResForm()
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-#     if form.validate_on_submit():
-#         restaurant.name = form.name.data
-#         session.commit()
-#         flash('Restaurant successfully edited.')
-#         return redirect(url_for('showRestaurants'))
-
-#     return render_template('editrestaurant.html',
-#                            form=form,
-#                            name=restaurant.name)
+    return render_template('newcategory.html', form=form)
 
 
-# @app.route('/restaurant/<int:restaurant_id>/delete', methods=['GET', 'POST'])
-# def deleteRestaurant(restaurant_id):
-#     form = DeleteForm()
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-#     if form.validate_on_submit():
-#         session.delete(restaurant)
-#         session.commit()
-#         flash('Restaurant successfully deleted')
-#         return redirect(url_for('showRestaurants'))
+@app.route('/category/<int:category_id>/edit', methods=['GET', 'POST'])
+def editCategory(category_id):
 
-#     return render_template('deleterestaurant.html',
-#                            form=form,
-#                            name=restaurant.name)
+    form = EditCategoryForm()
+    category = session.query(Category).filter_by(id=category_id).one()
+    if form.validate_on_submit():
+        category.name = form.name.data
+        session.commit()
+        flash('Category successfully edited.')
+        return redirect(url_for('mainPage'))
 
-
-# @app.route('/restaurant/<int:restaurant_id>')
-# @app.route('/restaurant/<int:restaurant_id>/menu')
-# def showMenu(restaurant_id):
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-#     items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
-#     if not items:
-#         flash('Menu item list is empty.')
-#     return render_template('menu.html', restaurant=restaurant, items=items)
+    return render_template('editcategory.html',
+                           form=form,
+                           name=category.name)
 
 
-# @app.route('/restaurant/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
-# def newMenuItem(restaurant_id):
-#     form = AddMenuItemForm()
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+@app.route('/category/<int:category_id>/delete', methods=['GET', 'POST'])
+def deleteCategory(category_id):
+    form = DeleteForm()
+    category = session.query(Category).filter_by(id=category_id).one()
+    if form.validate_on_submit():
+        session.delete(category)
+        session.commit()
+        flash('Category successfully deleted')
+        return redirect(url_for('mainPage'))
 
-#     if form.validate_on_submit():
-#         item = MenuItem(name=form.name.data,
-#                         course=form.course.data,
-#                         description=form.description.data,
-#                         price=form.price.data,
-#                         restaurant_id=restaurant_id)
-#         session.add(item)
-#         session.commit()
-#         flash('New menu item created.')
-#         return redirect(url_for('showMenu', restaurant_id=restaurant_id))
-
-#     return render_template('newmenuitem.html',
-#                            form=form,
-#                            restaurant=restaurant)
+    return render_template('deletecategory.html',
+                           form=form,
+                           name=category.name)
 
 
-# @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit',
-#            methods=['GET', 'POST'])
-# def editMenuItem(restaurant_id, menu_id):
-#     form = EditMenuItemForm()
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-#     item = session.query(MenuItem).filter_by(id=menu_id).one()
-
-#     if form.validate_on_submit():  # How to assign values more consicely
-#         item.name = form.name.data
-#         item.course = form.course.data
-#         item.description = form.description.data
-#         item.price = form.price.data
-
-#         session.commit()
-#         flash('Menu item successfully edited.')
-#         return redirect(url_for('showMenu', restaurant_id=restaurant_id))
-
-#     form.name.data = item.name
-#     form.course.data = item.course
-#     form.description.data = item.description
-#     form.price.data = item.price
-
-#     return render_template('editmenuitem.html',
-#                            form=form,
-#                            restaurant=restaurant,
-#                            item=item)
+@app.route('/category/<int:category_id>')
+def showCategory(category_id):
+    category = session.query(Category).filter_by(id=category_id).one_or_none()
+    items = session.query(Item).filter_by(category_id=category_id).all()
+    if not items or not category:
+        flash("Category is empty or doesn't exist.")
+    return render_template('showcategory.html', category=category, items=items)
 
 
-# @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete',
-#            methods=['GET', 'POST'])
-# def deleteMenuItem(restaurant_id, menu_id):
-#     form = DeleteForm()
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-#     item = session.query(MenuItem).filter_by(id=menu_id).one()
+@app.route('/category/<int:category_id>/items/new', methods=['GET', 'POST'])
+def newItem(category_id):
+    form = AddItemForm()
+    category = session.query(Category).filter_by(id=category_id).one()
 
-#     if form.validate_on_submit():
-#         session.delete(item)
-#         session.commit()
-#         flash('Menu item successfully deleted.')
-#         return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    if form.validate_on_submit():
+        item = Item(name=form.name.data,
+                    description=form.description.data,
+                    category=category)
+        session.add(item)
+        session.commit()
+        flash('New item created.')
+        return redirect(url_for('showCategory', category_id=category_id))
 
-#     return render_template('deletemenuitem.html',
-#                            form=form,
-#                            restaurant=restaurant,
-#                            item=item)
-
-
-# @app.route('/restaurants/JSON')
-# def restaurantsJSON():
-#     restaurants = session.query(Restaurant).all()
-#     return jsonify(Restaurants=[r.serialize for r in restaurants])
+    return render_template('newitem.html',
+                           form=form,
+                           category=category)
 
 
-# @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
-# def menusJSON(restaurant_id):
-#     menus = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
-#     return jsonify(MenuItems=[menu.serialize for menu in menus])
+@app.route('/category/<int:category_id>/items/<int:item_id>/edit',
+           methods=['GET', 'POST'])
+def editItem(category_id, item_id):
+    form = EditItemForm()
+    category = session.query(Category).filter_by(id=category_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
+
+    if form.validate_on_submit():  # How to assign values more consicely (setattr?)
+        item.name = form.name.data
+        item.description = form.description.data
+
+        session.commit()
+        flash('Item successfully edited.')
+        return redirect(url_for('showCategory', category_id=category_id))
+
+    form.name.data = item.name
+    form.description.data = item.description
+
+    return render_template('edititem.html',
+                           form=form,
+                           category=category,
+                           item=item)
 
 
-# @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-# def menuItemJSON(restaurant_id, menu_id):
-#     menu = session.query(MenuItem).filter_by(id=menu_id).one_or_none()
-#     return jsonify(MenuItem=[menu.serialize])
+@app.route('/category/<int:category_id>/items/<int:item_id>/delete',
+           methods=['GET', 'POST'])
+def deleteItem(category_id, item_id):
+    form = DeleteForm()
+    category = session.query(Category).filter_by(id=category_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
+
+    if form.validate_on_submit():
+        session.delete(item)
+        session.commit()
+        flash('Item successfully deleted.')
+        return redirect(url_for('showCategory', category_id=category_id))
+
+    return render_template('deleteitem.html',
+                           form=form,
+                           category=category,
+                           item=item)
+
+
+# @app.route('/categorys/JSON')
+# def categorysJSON():
+#     categorys = session.query(Category).all()
+#     return jsonify(Categorys=[r.serialize for r in categorys])
+
+
+# @app.route('/category/<int:category_id>/menu/JSON')
+# def menusJSON(category_id):
+#     menus = session.query(Item).filter_by(category_id=category_id).all()
+#     return jsonify(Items=[menu.serialize for menu in menus])
+
+
+# @app.route('/category/<int:category_id>/menu/<int:item_id>/JSON')
+# def menuItemJSON(category_id, item_id):
+#     menu = session.query(Item).filter_by(id=item_id).one_or_none()
+#     return jsonify(Item=[menu.serialize])
 
 
 if __name__ == '__main__':

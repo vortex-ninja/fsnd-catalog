0d1a490544d765d48da564a73afe609c7e1f6bfb
vortex-ninja <vortex.ninja@protonmail.com>
2017-07-27 14:28:50 +0200

    feat: Add User model

--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,31 @@ from sqlalchemy.sql import func
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(), nullable=False)
+    email = Column(String())
+
+    # @staticmethod
+    # def getUserID(email):
+    #     user = session.query(User).filter_by(email=email).one_or_none()
+    #     if 
+
+
+
+
+    # @staticmethod
+    # def createUser(login_sesssion):
+    #     new_user = User(name=login_sesssion['username'],
+    #                     email=login_sesssion['email'])
+    #     session.add(new_user)
+    #     session.commit()
+
+    #     return getUserID(login_sesssion['email'])
+
+
 class Category(Base):
     __tablename__ = 'category'
 
@@ -27,7 +52,7 @@ class Item(Base):
     name = Column(String(), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String())
-    date_created = Column(DateTime(timezone=True), server_default=func.now())
+    date_created = Column(DateTime(timezone=True), server_default=func.now()) # doesn't work properly
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
 
@@ -38,7 +63,7 @@ class Item(Base):
             'description': self.description,
             'id': self.id,
             'cat_id': self.category_id,
-            'date_created': self.date_created,
+            'date_created': self.date_created
         }
 
 

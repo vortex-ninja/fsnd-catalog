45ec88293c09ad5eead3beeb67500adde7e46fd2
vortex-ninja <vortex.ninja@protonmail.com>
2017-07-27 14:30:13 +0200

    feat: Add login functionality

--- final-project.py
+++ final-project.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, url_for, redirect, flash
-from flask import jsonify
+from flask import jsonify, request
+from flask import session as login_session
 from flask_bootstrap import Bootstrap
 from forms import CreateCategoryForm, EditCategoryForm, DeleteForm
 from forms import AddItemForm, EditItemForm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Item
+from database_setup import Base, Category, Item, User
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+import random
+import string
+import httplib2
+import json
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -16,11 +22,140 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# Get client id from json secret file
+
+with open('client_secrets.json', 'r') as f:
+    CLIENT_ID = json.loads(f.read())['web']['client_id']
+
+
+
+@app.route('/login')
+def login():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=login_session['state'])
+
+@app.route('/gconnect', methods=['POST', 'GET'])
+def gconnect():
+
+    # This view returns values to ajax call made from login.html
+    # All return values are therefore strings and not Response objects
+
+    # CSRF protection
+    if request.args.get('state') != login_session['state']:
+        return 'Invalid state parameter'
+
+    # Get code sent by ajax request from login.html signInCallback function
+    code = request.data
+
+    try:
+        # Create flow object from client_secrets.json
+
+        flow = flow_from_clientsecrets('client_secrets.json',
+                                       scope='',
+                                       redirect_uri='postmessage')
+
+        # Exchange an authorization code for a Credentials object
+        credentials = flow.step2_exchange(code)
+
+    except FlowExchangeError:
+        return 'Login error: failed to exchange authorization code'
+
+
+    # Check that the access token is valid
+
+    h = httplib2.Http()
+    access_token = credentials.access_token
+    url = credentials.token_info_uri + '?access_token={}'.format(access_token)
+
+
+
+    content = h.request(url)[1].decode('utf-8')
+    content = json.loads(content)
+
+
+    error = content.get('error_description')
+    if error is not None:
+        return error
+
+    # Verify that the access token is used for the intended user
+
+    gplus_id = credentials.id_token['sub']
+
+    if gplus_id != content['sub']:
+        return "Token's user ID doesn't match given user ID."
+
+    # Verify that token was issued to the right application
+
+    if content['aud'] != CLIENT_ID:
+        return "Token's client ID doesn't match app's."
+
+
+    # Verify that user is not already logged in
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplusid = login_session.get('gplus_id')
+
+    if stored_credentials is not None and stored_gplusid is not None:
+        return 'User already logged in.'
+
+
+    # Store the access token for later use
+
+    login_session['credentials'] = credentials.to_json()
+    login_session['gplus_id'] = gplus_id
+    login_session['provider'] = 'google'
+
+    # Get user info
+
+    api_url = 'https://www.googleapis.com/oauth2/v2/userinfo'
+    h = httplib2.Http()
+    userinfo_url = api_url + '?access_token={}'.format(access_token)
+    data = h.request(userinfo_url)[1].decode('utf-8')
+    data = json.loads(data)
+
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+
+    user_id = getUserID(login_session['email'])
+
+    if user_id is None:
+        user_id = createUser(login_session)
+
+    login_session['user_id'] = user_id
+
+    return 'Login Successfull.'
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        flash("You're not logged in.")
+        return redirect(url_for('mainPage'))
+
+
+    # revoke_url = credentials['revoke_uri']
+    # access_token = credentials['access_token']
+    # url = '{}?token={}'.format(revoke_url, access_token)
+    # h = httplib2.Http()
+    # result = h.request(url)[0]
+
+    del login_session['credentials']
+    del login_session['gplus_id']
+    del login_session['provider']
+    del login_session['username']
+    del login_session['email']
+    del login_session['user_id']
+
+    flash("You've been logged out.")
+    return redirect(url_for('mainPage'))
+
+
 @app.route('/')
 @app.route('/catalog')
 def mainPage():
     categories = session.query(Category).all()
-    items = session.query(Item).limit(5)
+    items = session.query(Item).limit(10)
     if not categories and not items:
         flash('No items to show')
     return render_template('main.html', categories=categories, items=items)
@@ -142,6 +277,11 @@ def deleteItem(category_id, item_id):
                            category=category,
                            item=item)
 
+@app.route('/category/<int:category_id>/items/<int:item_id>')
+def showItem(category_id, item_id):
+    item = session.query(Item).filter_by(id=item_id).one_or_none()
+    return render_template('showitem.html', item=item)
+
 
 # @app.route('/categorys/JSON')
 # def categorysJSON():
@@ -159,6 +299,20 @@ def deleteItem(category_id, item_id):
 # def menuItemJSON(category_id, item_id):
 #     menu = session.query(Item).filter_by(id=item_id).one_or_none()
 #     return jsonify(Item=[menu.serialize])
+
+
+# User functions
+
+def getUserID(email):
+    return session.query(User.id).filter_by(email=email).scalar()
+
+def createUser(login_session):
+    new_user = User(name=login_session['username'], email=login_session['email'])
+    session.add(new_user)
+    session.commit()
+    return new_user.id
+
+
 
 
 if __name__ == '__main__':

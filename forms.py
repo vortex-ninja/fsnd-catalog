01e2aa7760fe43a7eff59bcbe320e34bac3a0faa
vortex-ninja <vortex.ninja@protonmail.com>
2017-09-26 14:07:08 +0200

    refactor: Clean forms file

--- forms.py
+++ forms.py
@@ -1,6 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item, User
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 class CreateCategoryForm(FlaskForm):
@@ -19,13 +28,14 @@ class DeleteForm(FlaskForm):
     submit = SubmitField('Delete')
 
 
-class AddItemForm(FlaskForm):
+class EditItemForm(FlaskForm):
 
     name = StringField('Name', validators=[Required()])
     description = StringField('Description')
-    submit = SubmitField('Add')
-
-
-class EditItemForm(AddItemForm):
-
     submit = SubmitField('Edit')
+
+
+class AddItemForm(EditItemForm):
+
+    category = SelectField('Category', coerce=int)
+    submit = SubmitField('Add')

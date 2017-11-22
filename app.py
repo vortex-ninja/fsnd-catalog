26a2353dbe0fbe49b3f83d66c7bbf250925b2d22
vortex-ninja <vortex.ninja@protonmail.com>
2017-11-22 19:07:00 +0100

    Remove db setup config for Heroku

--- app.py
+++ app.py
@@ -18,75 +18,6 @@ import json
 import os
 import hashlib
 
-# database setup
-
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
-
-
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'user'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    email = Column(String)
-
-
-class Category(Base):
-    __tablename__ = 'category'
-
-    name = Column(String(), nullable=False)
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-    @property
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'user_id': self.user_id,
-        }
-
-
-class Item(Base):
-    __tablename__ = 'item'
-
-    name = Column(String(), nullable=False)
-    id = Column(Integer, primary_key=True)
-    description = Column(String())
-    date_created = Column(DateTime(timezone=True), server_default=func.now())
-    category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-    @property
-    def serialize(self):
-        return {
-            'name': self.name,
-            'description': self.description,
-            'id': self.id,
-            'cat_id': self.category_id,
-            'date_created': self.date_created,
-            'user_id': self.user_id,
-        }
-
-
-# This should go at the end of the file
-
-engine = create_engine(os.environ["DATABASE_URL"])
-Base.metadata.create_all(engine)
-
-
-# end of database setup
-
-
 
 app = Flask(__name__)
 app.config['PREFERRED_URL_SCHEME'] = os.environ["PREFERRED_URL_SCHEME"]
@@ -98,13 +29,6 @@ engine = create_engine(os.environ["DATABASE_URL"])
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
-
-
-
-
-# Get client id from json secret file
-
 
 CLIENT_ID = os.environ["CLIENT_ID"]
 

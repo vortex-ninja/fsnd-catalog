840392ab396512a3a19fd600caec601e40f4d72f
vortex-ninja <vortex.ninja@protonmail.com>
2017-11-27 15:41:20 +0100

    Fix link to a database

--- database_seed.py
+++ database_seed.py
@@ -5,9 +5,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Category, Base, Item, User
+import os
 
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine(os.environ["DATABASE_URL"])
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
